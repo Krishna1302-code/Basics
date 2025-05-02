0b35c49c11295a32344458fe1fdd160a0e66c5f2
Krishna <krishna.joshi@simformsolutions.com>
2025-05-02 18:21:07 +0530

    just adding the program

--- String-01.py
+++ String-01.py
@@ -1,1 +1,9 @@
-hello
+#Q-1 Write a function that counts the number of vowels in a string.
+def count_vowels1(name):
+    count=0
+    for i in range(len(name)):
+        if name[i].lower() in 'aeiou':
+            count =+ 1
+    print("No of vowels:",count)
+name='Apple'
+count_vowels1(name)
